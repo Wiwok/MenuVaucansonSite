45b54b067cc09a1646deaee3392c17b3acddd0b0
Wiwok <Emil.dechenaud@gmail.com>
2022-03-25 08:16:41 +0100

    First commit
    So, this is the first commit of this repo.

---
+++ script.js
@@ -0,0 +1,71 @@
+const today = new Date();
+const days = ['', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi'];
+let day = today.getDay();
+let date;
+
+if (day == 7) {
+	day = 2;
+	date = menu[0].entree;
+}
+
+else if (day == 6) {
+	day = 2;
+	date = menu[0].entree;
+}
+
+else if (day == 5) {
+	date = menu[0].dessert;
+}
+
+else if (day == 4) {
+	date = menu[0].laitier;
+}
+
+else if (day == 3) {
+	date = menu[0].legume;
+}
+
+else if (day == 2) {
+	date = menu[0].plat;
+}
+
+else if (day == 1) {
+	date = menu[0].entree;
+}
+
+
+var select = document.getElementById('list');
+
+function logValue() {
+    switch (this.value) {
+		case '0':
+			window.location.href='./index.html';
+			break;
+        case '1':
+			window.location.href='./lundi.html';
+            break;
+		case '2':
+			window.location.href='./mardi.html';
+            break;
+        case '3':
+			window.location.href='./mercredi.html';
+            break;
+		case '4':
+			window.location.href='./jeudi.html';
+			break;
+		case '5':
+			window.location.href='./vendredi.html';
+			break;
+    }
+}
+
+select.addEventListener('change', logValue, false);
+
+
+document.getElementById('jsEntree').innerHTML = menu[day + 1].entree;
+document.getElementById('jsPlat').innerHTML = menu[day + 1].plat;
+document.getElementById('jsLegume').innerHTML = menu[day + 1].legume;
+document.getElementById('jsLaitier').innerHTML = menu[day + 1].laitier;
+document.getElementById('jsDessert').innerHTML = menu[day + 1].dessert;
+document.getElementById('jsTitle').innerHTML = 'Menu du ' + days[day] + ' ' + date;
+document.getElementById('jsTitle').style.color = '#2f2f2f';
---
+++ scriptmardi.js
@@ -0,0 +1,50 @@
+const today = new Date();
+const days = ['', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi'];
+const Days = ['', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi'];
+const day = 2;
+const date = menu[0].plat;
+
+for (let i = 1; i < Days.length; i++){
+	document.write("<option class=\"options\" value=\""+i+"\">"+Days[i]+"</option>");
+}
+document.write("</select>");
+document.write("</form>");
+document.write("</div>");
+document.write("</div>");
+
+
+var select = document.getElementById('list');
+
+function logValue() {
+    switch (this.value) {
+		case '0':
+			window.location.href='./index.html';
+			break;
+        case '1':
+			window.location.href='./lundi.html';
+            break;
+		case '2':
+			window.location.href='./mardi.html';
+            break;
+        case '3':
+			window.location.href='./mercredi.html';
+            break;
+		case '4':
+			window.location.href='./jeudi.html';
+			break;
+		case '5':
+			window.location.href='./vendredi.html';
+			break;
+    }
+}
+
+select.addEventListener('change', logValue, false);
+
+
+document.getElementById('jsEntree').innerHTML = menu[day + 1].entree;
+document.getElementById('jsPlat').innerHTML = menu[day + 1].plat;
+document.getElementById('jsLegume').innerHTML = menu[day + 1].legume;
+document.getElementById('jsLaitier').innerHTML = menu[day + 1].laitier;
+document.getElementById('jsDessert').innerHTML = menu[day + 1].dessert;
+document.getElementById('jsTitle').innerHTML = 'Menu du ' + days[day] + ' ' + date;
+document.getElementById('jsTitle').style.color = '#2f2f2f';
